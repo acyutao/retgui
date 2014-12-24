51c72e022572fd87c1137c2ba54f7481dd0ebf32
newjob2015 <newjob_2015@163.com>
2014-12-24 15:48:38 +0800

    test 2

--- retgui/src/main/java/org/acca/retgui/App.java
+++ retgui/src/main/java/org/acca/retgui/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Zhang Xue!" );
+        System.out.println( "Zhang Xue another account !" );
     }
 }
