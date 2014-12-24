47acc6a3a4c5f11ee715b7961b6c9e900a8ebfa4
acyutao <ac_yutao@126.com>
2014-12-24 16:26:36 +0800

    远程更新

--- retgui/src/main/java/org/acca/retgui/App.java
+++ retgui/src/main/java/org/acca/retgui/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Zhang Xue!" );
         System.out.println( "Zhang Xue another account !" );
+        
+        System.out.println( "远程更新" );
     }
 }
